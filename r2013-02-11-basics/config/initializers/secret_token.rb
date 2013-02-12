936cfcdbfa2fc03fa500f032a33efa2b1b3581ab
Adi Singh <adityasingh89@gmail.com>
2013-02-12 10:24:35 -0500

    rails practice

---
+++ r2013-02-11-basics/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+R20130211Basics::Application.config.secret_token = 'fff4b786ae4cc8419f322df9925f9ae7e348bcfcf0dd87299bd67bd5d8b0d37e1876604750bd3cc588443f796bb162885584d87b74bf4e585f67b8397acfa12f'
---
+++ r2013-02-11-routing/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+R20130211Routing::Application.config.secret_token = '18e4d4561af339eee9144cf0a4f6cf3272758e18a017e591c0212df6fc4998c0348c798570986ea9108bc957887f8ba1392fa096e96dfe26f2517d3e4d9c2c5e'
